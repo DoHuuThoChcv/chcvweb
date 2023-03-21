2624a8ce00eec4f37823ededb68022e24494c2bf
CHCV\huutho27 <dht.huutho@gmail.com>
2023-03-21 14:22:27 +0700

    no message

--- js/i18n_1.2.js
+++ js/i18n_1.2.js
@@ -43,7 +43,7 @@ $(function () {
                         },
                         //----------------------------- Menu Navigation ------------------------------
                         menu: {
-                            information: 'Thông tin CHCV123',
+                            information: 'Thông tin CHCV',
                             information_sub1: 'Lịch sử hình thành',
                             information_sub2: 'Năng lực phân phối',
                             information_sub3: 'Năng lực sản xuất',
